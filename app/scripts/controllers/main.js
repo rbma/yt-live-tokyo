a49f0efb5aab1130eff1ad680061cf515b6a4f5f
James Pants <jamespants@gmail.com>
2014-10-31 21:24:29 +0900

    first commit

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name youtubeStreamApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the youtubeStreamApp
+ */
+angular.module('youtubeStreamApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
